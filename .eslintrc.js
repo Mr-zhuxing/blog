fe28ebbc865d036c6c3dbe9b5f8c53672ca5362e
zhu_xing <1172254048@qq.com>
2022-07-06 11:13:39 +0800

    Keep calm and commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,6 @@ module.exports = {
   extends: [require.resolve("@umijs/fabric/dist/eslint")],
   rules: {
     // "no-console": "error",
-    // "no-debugger": "error",
+    "no-debugger": "error",
   },
 };
