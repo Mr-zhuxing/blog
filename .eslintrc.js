0b61e1a8016b08841d140e714fafcca9e708c326
zhu_xing <1172254048@qq.com>
2022-07-06 11:51:53 +0800

    Keep calm and commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,3 @@
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true,
-//   },
-//   extends: [
-//     "eslint:recommended",
-//     "plugin:react/recommended",
-//     "plugin:@typescript-eslint/recommended",
-//   ],
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     ecmaFeatures: {
-//       jsx: true,
-//     },
-//     ecmaVersion: "latest",
-//     sourceType: "module",
-//   },
-//   plugins: ["react", "@typescript-eslint"],
-//   rules: {
-//     "no-console": ["error"],
-//     "no-debugger": 1,
-//   },
-// };
-
 module.exports = {
   extends: [require.resolve("@umijs/fabric/dist/eslint")],
   // rules: {
